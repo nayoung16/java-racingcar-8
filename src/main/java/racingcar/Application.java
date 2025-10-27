3e354ca1f15471ced3277d8ef804da60182114f9
nayoung16 <nykim1016@sookmyung.ac.kr>
2025-10-27 13:01:46 +0900

    feat(application) : 최종 기능 구현 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,18 +1,10 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-import racingcar.validation.CarNameValidator;
-import racingcar.validation.TryCountValidator;
-
-import java.util.List;
+import racingcar.controller.RacingController;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNames = Console.readLine();
-        List<String> validatedNames = CarNameValidator.validate(carNames);
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String tryCount = Console.readLine();
-        int validatedNum = TryCountValidator.validate(tryCount);
+        RacingController racingController = new RacingController();
+        racingController.run();
     }
 }
