3e1031ee0bf5d7e6ce6cb99bfe815b1994530e22
nayoung16 <nykim1016@sookmyung.ac.kr>
2025-10-27 12:29:25 +0900

    feat(view) : 자동차 이름, 횟수 입력 view 개발 및 검증

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,21 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.validation.CarNameValidator;
+import racingcar.validation.TryCountValidator;
+
+import java.util.List;
+
+public class InputView {
+    public static List<String> inputCarNames() {
+        System.out.println("경주할 자동차 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+        return CarNameValidator.validate(carNames);
+    }
+
+    public static int inputTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String tryCount = Console.readLine();
+        return TryCountValidator.validate(tryCount);
+    }
+}
